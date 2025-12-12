d84f39434636880d31c31cc0457d0b9d7c5af6aa
Bnarendra517 <narendra.balla@gmail.com>
2025-12-12 18:26:32 +0530

    change in spec commit

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,6 @@
 import {test,expect} from '@playwright/test';
 test('My test', async ({ page }) => {
+  console.log("Execution started.......");
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await expect(page).toHaveTitle(/OrangeHRM/);
   await page.locator('input[name="username"]').fill('Admin');
