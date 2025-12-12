3f62511797780c5b4da2f0ea9a08c6e4018e53b8
Bnarendra517 <narendra.balla@gmail.com>
2025-12-12 16:16:04 +0530

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import {test,expect} from '@playwright/test';
+test('My test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await expect(page).toHaveTitle(/OrangeHRM/);
+  await page.locator('input[name="username"]').fill('Admin');
+  await page.locator('input[name="password"]').fill('admin123');
+  await page.locator('button[type="submit"]').click();
+  await expect(page).toHaveURL(/dashboard/);  
+
+
+});
